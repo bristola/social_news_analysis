b1f15d297741c92f3c956c5f445011e287299bf4
bristola <bristola@allegheny.edu>
2019-01-16 22:00:20 -0500

    Added ability to get more than 100 tweets

--- src/project/data_collector/twitter_collector.py
+++ src/project/data_collector/twitter_collector.py
@@ -1,5 +1,6 @@
 import twitter
 from data_collector import Data_Collector
+import time
 
 class Twitter_Collector(Data_Collector):
 
@@ -13,19 +14,24 @@ class Twitter_Collector(Data_Collector):
     def search(self, topic, max=None, c=100):
         results = self.api.GetSearch(term=topic, max_id=max, count=c, result_type="recent", lang='en')
         tweets = list()
+        min_id = None
         for tweet in results:
             current = dict()
             current['favorites'] = tweet.favorite_count
             current['retweets'] = tweet.retweet_count
             current['text'] = tweet.text
             current['user'] = tweet.user.screen_name
+            current['id'] = tweet.id
+            if min_id == None or int(tweet.id) < min_id:
+                min_id = int(tweet.id)
             tweets.append(current)
-        return tweets
+        return tweets, min_id-1
 
 
     def run(self, topic):
-        # TODO: Max API query is 100 tweets. Need to iterate as many times as
-        # the API will allow. Each time, query for the most recent tweets, and
-        # take the lowest ID from the data. Use this ID as the max_id for the
-        # next iteration.
-        return
+        iterations = 2
+        min_id = None
+        tweets = list()
+        for i in range(0, iterations):
+            cur_tweets, min_id = self.search(topic, max=min_id)
+            tweets.extend(cur_tweets)
