6f14523d572868a944bef4d101c05b3627ce1c98
bristola <bristola@allegheny.edu>
2019-01-18 12:25:14 -0500

    Added abstract class

--- src/project/data_collector/data_collector.py
+++ src/project/data_collector/data_collector.py
@@ -1,4 +1,6 @@
-class Data_Collector:
+from abc import ABC, abstractmethod
+
+class Data_Collector(ABC):
 
     def __init__(self, out_file):
         self.output = out_file
@@ -7,3 +9,15 @@ class Data_Collector:
     def write_to_file(self, data):
         with open(self.output, 'w+', encoding="utf-8") as f:
             f.write("\n".join(data))
+
+    @abstractmethod
+    def search(self, topic, max=None, c=100):
+        pass
+
+    @abstractmethod
+    def create_strings(self, data):
+        pass
+
+    @abstractmethod
+    def run(self, topic, iterations):
+        pass
