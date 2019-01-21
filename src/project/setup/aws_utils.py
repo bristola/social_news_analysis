49c266e06a607810a7ca986a8869de5988feb066
bristola <bristola@allegheny.edu>
2019-01-21 15:23:22 -0500

    Initial functions for AWS Utils

--- src/project/setup/aws_utils.py
+++ src/project/setup/aws_utils.py
@@ -1,15 +1,35 @@
-
+import boto3
+import time
 
 # pip install awscli boto3
 
 class AWS_Utils:
 
-    def __init__(self, key_name, machine_type, security_group, image_id, absolute_path_to_pem):
+    def __init__(self, key_name, machine_type, security_group, image_id):
         self.key_name = key_name
         self.machine_type = machine_type
-        self.security_group = security_group
+        self.security_group = [security_group]
         self.image_id = image_id
-        self.absolute_path_to_pem = absolute_path_to_pem
+        self.ec2 = boto3.resource('ec2', region_name='us-east-1')
 
-    def create_instance():
-        pass
+
+    def create_instance(self, number):
+        instances = self.ec2.create_instances(ImageId=self.image_id,
+                                  InstanceType=self.machine_type,
+                                  KeyName=self.key_name,
+                                  SecurityGroupIds=self.security_group,
+                                  MinCount=number,
+                                  MaxCount=number)
+        ids = [instance.id for instance in instances]
+        time.sleep(30)
+        ips = self.get_public_ips(ids)
+        return ids, ips
+
+
+    def get_public_ips(self, ids):
+        instances = self.ec2.instances.filter()
+        ips = list()
+        for instance in instances:
+            if instance.id in ids:
+                ips.append(instance.public_ip_address)
+        return ips
--- src/project/setup/instance_setup.py
+++ src/project/setup/instance_setup.py
@@ -1,1 +1,2 @@
 # Use AWS API to start up all the instances, transfer code, and install packages
+from aws_utils import AWS_Utils
