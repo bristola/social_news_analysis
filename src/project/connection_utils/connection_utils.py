6149c0c6c4da28bf188a073295e77ea2f281d846
bristola <bristola@allegheny.edu>
2019-01-22 15:49:13 -0500

    Added some ssh and scp code

--- src/project/connection_utils/connection_utils.py
+++ src/project/connection_utils/connection_utils.py
@@ -1,1 +1,26 @@
-# Contains code that uses SSH and SCP to execute commands
+from paramiko import SSHClient
+from scp import SCPClient
+
+# pip install paramiko
+
+class Connection_Utils:
+
+    def __init__(self, pem_location, username="ubuntu"):
+        self.pem_location = pem_location
+        self.username = username
+        self.client = SSHClient()
+
+
+    def run_commands(self, ip, commands):
+        self.client.connect(ip, username=self.username, pkey=self.pem_location)
+        for command in commands:
+            stdin, stdout, stderr = self.client.exec_command(command)
+            stdout.channel.recv_exit_status()
+        self.client.close()
+
+
+    def transfer_files(self, ip, files, destinations):
+        self.client.connect(ip, username=self.username, pkey=self.pem_location)
+        with SCPClient(self.client.get_transport()) as scp_conn:
+            for file, destination in zip(files, destinations):
+                scp_conn.put(file, destination)
