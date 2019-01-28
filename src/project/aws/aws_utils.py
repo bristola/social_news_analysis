7f0665725711614a9e1c75eec1b75cc6cc0326bc
bristola <bristola@allegheny.edu>
2019-01-27 22:19:29 -0500

    Small change with new account

--- src/project/aws/aws_utils.py
+++ src/project/aws/aws_utils.py
@@ -10,8 +10,8 @@ class AWS_Utils:
         self.machine_type = machine_type
         self.security_group = [security_group]
         self.image_id = image_id
-        self.ec2 = boto3.resource('ec2', region_name='us-east-1')
-        self.ec2_client = boto3.client('ec2', region_name='us-east-1')
+        self.ec2 = boto3.resource('ec2', region_name='us-east-2')
+        self.ec2_client = boto3.client('ec2', region_name='us-east-2')
 
 
     def create_instance(self, number):
