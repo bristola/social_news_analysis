1cdd7eae3cc3d324372828952f15900d781f130f
bristola <bristola@allegheny.edu>
2019-02-04 22:04:57 -0500

    Redo of sentiment analysis work

--- src/project/analysis/analysis.py
+++ src/project/analysis/analysis.py
@@ -1,19 +1,31 @@
-import pickle
-from textblob import TextBlob
-from textblob.classifiers import NaiveBayesClassifier
+from nltk import download
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
+from nltk.stem import WordNetLemmatizer
+import json
 
-cl = None
+class Analyzer:
 
-with open("trained_classifier.pickle", "rb") as trained_classifier:
-    print(trained_classifier)
-    cl = pickle.load(trained_classifier)
+    def __init__(self, extra_words_file='extra_words.json'):
+        download('punkt')
+        download('wordnet')
+        download('vader_lexicon')
+        self.lemmatizer = WordNetLemmatizer()
+        self.sentiment_analyzer = SentimentIntensityAnalyzer()
+        with open(extra_words_file, 'r') as extra_words:
+            words = json.loads(extra_words.read())
+            print(words)
+            self.sentiment_analyzer.lexicon.update(words)
 
-print(cl.classify("This is awesome!"))
+    def get_sentiment(self, data):
+        sentiment_total = 0
+        sentances = tokenize.sent_tokenize(data)
+        for sentance in sentances:
+            lemmas = [self.lemmatizer.lemmatize(word) for word in sentance.split(" ")]
+            sentance = ' '.join(lemmas)
+            sentiment = self.sentiment_analyzer.polarity_scores(sentance)
+            sentiment_total += sentiment['compound']
+        return sentiment_total / len(sentances)
 
-# BREAK INTO BLOBS
-
-# TOKENIZE
-
-# LEMMATIZE
-
-# CLASSIFY
+a = Analyzer()
+a.get_sentiment("This is a clown show or what?")
