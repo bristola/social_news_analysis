2ca38e97044c41aea46cdde9de6e30f4f30e3556
bristola <bristola@allegheny.edu>
2019-02-04 22:46:30 -0500

    Threading for data collection

--- src/project/aws/aws_runner.py
+++ src/project/aws/aws_runner.py
@@ -13,12 +13,13 @@ class AWS_Runner:
     def setup_instances(self):
         ids, ips = self.aws.create_instance(5)
 
-        parameters = list()
-        parameters.append((ips[0], commands.twitter_collector_cmd, commands.twitter_files, commands.twitter_destinations))
-        parameters.append((ips[1], commands.news_collector_cmd, commands.news_files, commands.news_destinations))
-        parameters.append((ips[2], commands.analytics_cmd, commands.analytics_files, commands.analytics_destinations))
-        parameters.append((ips[3], commands.analytics_cmd, commands.analytics_files, commands.analytics_destinations))
-        parameters.append((ips[4], commands.database_cmd, commands.database_files, commands.database_destinations))
+        parameters = [
+            (ips[0], commands.twitter_collector_cmd, commands.twitter_files, commands.twitter_destinations),
+            (ips[1], commands.news_collector_cmd, commands.news_files, commands.news_destinations),
+            (ips[2], commands.analytics_cmd, commands.analytics_files, commands.analytics_destinations),
+            (ips[3], commands.analytics_cmd, commands.analytics_files, commands.analytics_destinations),
+            (ips[4], commands.database_cmd, commands.database_files, commands.database_destinations)
+        ]
 
         pool = ThreadPool(5)
         pool.map(self.installation_thread, parameters)
@@ -29,14 +30,28 @@ class AWS_Runner:
 
 
     def execute_system(self, session, config, topic):
-        # Execute data collection processes
-        ip = session['Twitter Collector IP']
-        command = commands.data_exec_twitter % (topic, config['Twitter API key'], config['Twitter API secret key'], config['Twitter Access token'], config['Twitter Access token secret'])
-        self.conn.run_commands(ip, [command])
 
-        ip = session['News Collector IP']
-        command = commands.data_exec_news % (topic, config['News API key'])
-        self.conn.run_commands(ip, [command])
+        command1 = commands.data_exec_twitter % (topic, config['Twitter API key'], config['Twitter API secret key'], config['Twitter Access token'], config['Twitter Access token secret'])
+        command2 = commands.data_exec_news % (topic, config['News API key'])
+
+        parameters = [
+            (session['Twitter Collector IP'], [command1]),
+            (session['News Collector IP'], [command2])
+        ]
+
+        pool = ThreadPool(2)
+        pool.map(self.data_collection_thread, parameters)
+        pool.close()
+        pool.join()
+
+        # Execute data collection processes
+        # ip = session['Twitter Collector IP']
+        # command = commands.data_exec_twitter % (topic, config['Twitter API key'], config['Twitter API secret key'], config['Twitter Access token'], config['Twitter Access token secret'])
+        # self.conn.run_commands(ip, [command])
+        #
+        # ip = session['News Collector IP']
+        # command = commands.data_exec_news % (topic, config['News API key'])
+        # self.conn.run_commands(ip, [command])
 
         # Determine how many data analytics servers to create based on how many are running
 
@@ -53,6 +68,11 @@ class AWS_Runner:
         ids = [value for key, value in config.items() if "ID" in key]
         self.aws.end_instances(ids)
 
+
     def installation_thread(self, params):
         self.conn.run_commands(params[0], params[1])
         self.conn.transfer_files(params[0], params[2], params[3])
+
+
+    def data_collection_thread(self, params):
+        self.conn.run_commands(params[0], params[1])
