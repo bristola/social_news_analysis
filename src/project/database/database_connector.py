c02466d4fda72dab77a9a46f282950e17b60de69
bristola <bristola@allegheny.edu>
2019-02-08 13:27:41 -0500

    Started data connector code

--- src/project/database/database_connector.py
+++ src/project/database/database_connector.py
@@ -1,2 +1,38 @@
+import psycopg2
+
 class Database_Connector:
-    pass
+
+    def __init__(self, database_ip, database_name="postgres", database_user="postgres", database_password="admin"):
+        self.database_ip = database_ip
+        self.database_name = database_name
+        self.database_user = database_user
+        self.database_password = database_password
+
+
+    def execute_selection(self, select_str):
+        results = None
+        try:
+            conn = psycopg2.connect(dbname=self.database_name, host=self.database_ip, user=self.database_user, password=self.database_password)
+            with conn.cursor() as cur:
+                cur.execute(select_str)
+                results = cur.fetchall()
+            if conn is not None:
+                conn.close()
+        except Exception as e:
+            pass
+        return results
+
+
+    def get_sentiment_groups(self):
+        pass
+
+
+    def get_sentiment_totals(self):
+        pass
+
+
+    def get_mood_totals(self):
+        pass
+
+    def get_emoticon_totals(self):
+        pass
