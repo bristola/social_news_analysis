24508b0b94aaea73b4615f9aba563b5925283fcb
bristola <bristola@allegheny.edu>
2019-02-21 22:31:27 -0500

    Reverted js

--- src/project/static/js/loading.js
+++ src/project/static/js/loading.js
@@ -1,3 +1,6 @@
 function trigger_loading() {
-    document.getElementById("loading").style.visibility = "visible"
+    var div1 = document.getElementById("loading");
+    div1.style.visibility = div1.style.visibility == "hidden" ? "visible" : "hidden";
+    var div2 = document.getElementById("live")
+    div2.style.visibility = div2.style.visibility == "hidden" ? "visible" : "hidden";
 }
