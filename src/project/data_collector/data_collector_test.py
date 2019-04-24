dc0e6a9d0df7843a428d6669c86ad43e3b4fbda7
Austin Bristol <bristola@aldenv175.allegheny.edu>
2019-04-24 09:31:10 -0400

    A data collector test

--- src/project/data_collector/data_collector_test.py
+++ src/project/data_collector/data_collector_test.py
@@ -1,4 +1,18 @@
+import pytest
 from data_collector import *
+from twitter_collector import *
 
-def test_filter_authors():
-    assert True
+
+d1 = Twitter_Collector("","","","")
+
+
+@pytest.mark.parametrize(
+    "data,expected",
+    [
+        ([{"author":"author1","text":"Some text"}],[{"author":"author1","text":"Some text"}]),
+        ([{"author":"author1","text":"Some text"},{"author":"author1","text":"Another text"}],[{"author":"author1","text":"Some text"}])
+    ]
+)
+def test_filter_authors(data, expected):
+    out_data = d1.filter_authors(data)
+    assert out_data == expected
